0b32aa4e08a3e2cb2a2325d907de95477f85961c
VaniPed <136858162+VaniPed@users.noreply.github.com>
2023-06-23 11:20:23 -0300

    Add files via upload

---
+++ TP2 - 6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	int num=0, a1=1, a2=1, mem=0;
+	
+	printf("Ingrese un numero: ");
+	scanf ("%d", &num);
+	
+	for (int i=2; i<num; i++){
+		mem = a1 + a2;
+		a1 = a2;
+		a2 = mem;
+	}
+	
+	printf ("\n El valor de la sucesion para el numero ingresado es: %d", mem);
+	return 0;
+}
+
