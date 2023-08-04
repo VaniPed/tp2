25f4d7a373cb72657146dd79139323e01d9a8509
defli7 <136860280+defli7@users.noreply.github.com>
2023-08-04 18:09:51 -0300

    Add files via upload

---
+++ TP2-4 Villarreal y Alvarez.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main ()
+{ 
+	int valor = 0;
+	int factorial = 1;
+	
+	printf("ingrese el valor del factorial:");
+	scanf("%d", &valor);
+	
+	for (int i = 1; i <= valor; i++)
+	{
+		factorial = factorial * i;
+	}
+	printf("el factorial de %d es: %d", valor, factorial);
+	
+}
+
+
